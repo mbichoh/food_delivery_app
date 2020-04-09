6cdb4d67a685b2c288dd700f2992dabf35634ce0
mbichoh <nattymbichoh@gmail.com>
2020-04-09 12:18:16 +0300

    Forgot Password design

--- app/src/main/java/com/nathanmbichoh/food_delivery_app/LoginActivity.java
+++ app/src/main/java/com/nathanmbichoh/food_delivery_app/LoginActivity.java
@@ -2,6 +2,7 @@ package com.nathanmbichoh.food_delivery_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -44,7 +45,7 @@ public class LoginActivity extends AppCompatActivity {
         txtForgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startActivity(new Intent(LoginActivity.this, ForgotPasswordActivity.class));
             }
         });
 
