2539eb8c7dc8a2d54a2acc8dc12c311dc94ed72c
mbichoh <nattymbichoh@gmail.com>
2020-04-09 07:37:16 +0300

    Food delivery app

---
+++ app/src/main/java/com/nathanmbichoh/food_delivery_app/MainActivity.java
@@ -0,0 +1,27 @@
+package com.nathanmbichoh.food_delivery_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnGetStarted;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnGetStarted = (Button) findViewById(R.id.btngs);
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
